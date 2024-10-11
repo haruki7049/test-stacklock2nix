5757e6ad953d574ef448b90ab147ab7b6ae48b8a
haruki7049 <tontonkirikiri@gmail.com>
2024-10-11 23:36:21 +0900

    hoge

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hoge"
+import System.Console.CmdArgs
+
+data Sample = Sample {hello :: String}
+  deriving (Show, Data, Typeable)
+
+sample = Sample {hello = def}
+
+main = print =<< cmdArgs sample
