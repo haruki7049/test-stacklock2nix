62f445b7a63fa051be3cac8f16506eac5a4e63af
haruki7049 <tontonkirikiri@gmail.com>
2024-10-12 08:12:17 +0900

    format

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,24 @@ import Control.Applicative
 import Options
 
 data MainOptions = MainOptions
-  { optMessage :: String
-  , optQuiet :: Bool
+  { optMessage :: String,
+    optQuiet :: Bool
   }
 
 instance Options MainOptions where
-  defineOptions = pure MainOptions
-    <*> simpleOption "message" "Hello world!"
-          "A message to show the user."
-    <*> simpleOption "quiet" False
-         "Whether to be quiet."
+  defineOptions =
+    pure MainOptions
+      <*> simpleOption
+        "message"
+        "Hello world!"
+        "A message to show the user."
+      <*> simpleOption
+        "quiet"
+        False
+        "Whether to be quiet."
 
 main :: IO ()
 main = runCommand $ \opts args ->
   if optQuiet opts
-      then return ()
-      else putStrLn (optMessage opts)
+    then return ()
+    else putStrLn (optMessage opts)
