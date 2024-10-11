7d2ea48549261e216921ed81a4002ad2af462163
haruki7049 <tontonkirikiri@gmail.com>
2024-10-11 23:29:36 +0900

    format

--- flake.nix
+++ flake.nix
@@ -3,53 +3,66 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     stacklock2nix.url = "github:cdepillabout/stacklock2nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
 
-      perSystem = { pkgs, system, ... }:
-      let
-        overlays = [
-          inputs.stacklock2nix.overlay
-        ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
 
-        stacklock = pkgs.stacklock2nix {
-          stackYaml = ./stack.yaml;
-        };
-        haskell-pkg-set = pkgs.haskell.packages.ghcHEAD.override (oldAttrs: {
-          inherit (stacklock) all-cabal-hashes;
-
-          overrides = pkgs.lib.composeManyExtensions [
-            stacklock.stackYamlResolverOverlay
-            stacklock.stackYamlExtraDepsOverlay
-            stacklock.stackYamlLocalPkgsOverlay
-            stacklock.suggestedOverlay
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          overlays = [
+            inputs.stacklock2nix.overlay
           ];
-        });
-        test-stacklock2nix = haskell-pkg-set.test-stacklock2nix;
-      in
-      {
-        _module.args = import inputs.nixpkgs {
-          inherit system overlays;
-        };
 
-        packages = {
-          inherit test-stacklock2nix;
-          default = test-stacklock2nix;
-        };
+          stacklock = pkgs.stacklock2nix {
+            stackYaml = ./stack.yaml;
+          };
+          haskell-pkg-set = pkgs.haskell.packages.ghcHEAD.override (oldAttrs: {
+            inherit (stacklock) all-cabal-hashes;
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.stack
-            pkgs.cabal-install
-            pkgs.ghc
-            pkgs.haskell-language-server
-            pkgs.nil
-          ];
+            overrides = pkgs.lib.composeManyExtensions [
+              stacklock.stackYamlResolverOverlay
+              stacklock.stackYamlExtraDepsOverlay
+              stacklock.stackYamlLocalPkgsOverlay
+              stacklock.suggestedOverlay
+            ];
+          });
+          test-stacklock2nix = haskell-pkg-set.test-stacklock2nix;
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.ormolu.enable = true;
+          };
+
+          _module.args = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+
+          packages = {
+            inherit test-stacklock2nix;
+            default = test-stacklock2nix;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.stack
+              pkgs.cabal-install
+              pkgs.ghc
+              pkgs.haskell-language-server
+              pkgs.nil
+            ];
+          };
         };
-      };
     };
 }
